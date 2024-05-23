dc6929085fcabbf08b5413ea70c3e6173bd46455
Dallow2020 <dallasdevco@gmail.com>
2024-05-24 01:11:50 +0800

    initial file upload

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ from flask_migrate import Migrate
 from datetime import datetime
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'it-is-a-secret-key-for-test-purposes'
+Bootstrap5(app)
 class Intake(FlaskForm):
     fname = StringField('First Name', validators=[DataRequired()])
     mname = StringField('Middle Name')
     sname = StringField('Surname', validators=[DataRequired()])
     address = StringField('Address', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    state = StringField('State', validators=[DataRequired()])
+    postcode = StringField('Postcode', validators=[DataRequired()])
     dob = DateField('Date of Birth', validators=[DataRequired()])
-    allergies = RadioField('Allergies', choices=[("Yes"),('No')])
+    allergies = RadioField('Allergies', choices=[("Yes",'I do have allergies'),('No', 'No i do not have allergies')])
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/register')
+def register():
+    form = Intake()
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
